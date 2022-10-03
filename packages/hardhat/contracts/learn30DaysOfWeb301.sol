6e68551464623545c4390a8ce62f65c036cfeb6d
eduairet <hola@eduairet.com>
2022-10-03 15:31:39 -0500

    Intro contract

---
+++ packages/hardhat/contracts/learn30DaysOfWeb301.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Intro {
+
+  string public myIntro = "Hi, I'm Eduardo";
+
+  function setIntro( string memory intro ) public {
+    myIntro = intro;
+  }
+  function getIntro() public view returns( string memory ) {
+    return myIntro;
+  }
+
+}
